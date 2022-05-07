673b8eb01c8042db9e7381e4e7a68d1013a9e482
Praful <prafulrakhade02@gmail.com>
2022-05-07 23:34:43 +0530

    [praful] Added : UC1 Program File

---
+++ LineComparison/LineComparison.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LineComparison
+    {
+        double x1, y1, x2, y2;
+        double line1, line2;
+        double distance = 0;
+        public void Data()  // for following the dry principle I have create a repeatation code in one mathod and apply or use that method 
+        {
+
+            Console.WriteLine("Enter the line-1 Coordinates (x1 , y1) ");
+            x1 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the line-2 Coordinates (x2 , y2) ");
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+        }
+
+        public void UC_1_CalculationOfLenght()
+        {
+            Data();                 //its call method calling and follow the DRY principle  //class method       
+            Console.WriteLine("Enter the line-1 Co-ordinates (x1 , y1) ");
+            distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * ((y2 - y1)));          //Math.sqrt is a inbuild c
+            Console.WriteLine("The Distance of line 1 and line 2 = " + distance);
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LineComparison comparison = new LineComparison();
+            comparison.UC_1_CalculationOfLenght();
+        }
+    }
+}
